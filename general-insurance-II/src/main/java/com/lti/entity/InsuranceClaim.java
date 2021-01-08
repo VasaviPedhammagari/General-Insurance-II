af6c9127541c13db7f22fc9cf47e1065d9c71087
Sripratha <krishnasamykrs@gmail.com>
2021-01-08 22:06:11 +0530

    Added entity classes

--- general-insurance-II/src/main/java/com/lti/entity/InsuranceClaim.java
+++ general-insurance-II/src/main/java/com/lti/entity/InsuranceClaim.java
@@ -1,5 +1,85 @@
 package com.lti.entity;
 
-public class InsuranceClaim {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "insurance_claim")
+public class InsuranceClaim {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "claim_no")
+	private int claimNo;
+	
+	@Column(name = "claim_reason")
+	private String  claimReason;
+	
+	@Column(name = "claim_date")
+	private LocalDate claimDate;
+	
+	@Column(name = "claim_status")
+	private String claimStatus;
+	
+	@Column(name = "claim_amount")
+	private int claimAmount;
+	
+	@ManyToOne
+	@JoinColumn(name = "policy_no")
+	private MotorInsurance motorInsurance;
+
+	public int getClaimNo() {
+		return claimNo;
+	}
+
+	public void setClaimNo(int claimNo) {
+		this.claimNo = claimNo;
+	}
+
+	public String getClaimReason() {
+		return claimReason;
+	}
+
+	public void setClaimReason(String claimReason) {
+		this.claimReason = claimReason;
+	}
+
+	public LocalDate getClaimDate() {
+		return claimDate;
+	}
+
+	public void setClaimDate(LocalDate claimDate) {
+		this.claimDate = claimDate;
+	}
+
+	public String getClaimStatus() {
+		return claimStatus;
+	}
+
+	public void setClaimStatus(String claimStatus) {
+		this.claimStatus = claimStatus;
+	}
+
+	public int getClaimAmount() {
+		return claimAmount;
+	}
+
+	public void setClaimAmount(int claimAmount) {
+		this.claimAmount = claimAmount;
+	}
+
+	public MotorInsurance getMotorInsurance() {
+		return motorInsurance;
+	}
+
+	public void setMotorInsurance(MotorInsurance motorInsurance) {
+		this.motorInsurance = motorInsurance;
+	}
 }
--- general-insurance-II/src/main/java/com/lti/entity/Payment.java
+++ general-insurance-II/src/main/java/com/lti/entity/Payment.java
@@ -1,5 +1,96 @@
 package com.lti.entity;
 
-public class Payment {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "payment")
+public class Payment {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "payment_id")
+	private int paymentId;
+	
+	@Column(name = "insurance_price")
+	private int insurancePrice;
+	
+	@Column(name = "payment_mode")
+	private String paymentMode;
+	
+	@Column(name = "payment_status")
+	private String paymentStatus;
+	
+	@Column(name = "insurance_status")
+	private String insuranceStatus;
+	
+	@Column(name = "payment_date")
+	private LocalDate paymentDate;
+	
+	@OneToOne
+	@JoinColumn(name = "policy_no")
+	private MotorInsurance motorInsurance;
+
+	public int getPaymentId() {
+		return paymentId;
+	}
+
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+
+	public int getInsurancePrice() {
+		return insurancePrice;
+	}
+
+	public void setInsurancePrice(int insurancePrice) {
+		this.insurancePrice = insurancePrice;
+	}
+
+	public String getPaymentMode() {
+		return paymentMode;
+	}
+
+	public void setPaymentMode(String paymentMode) {
+		this.paymentMode = paymentMode;
+	}
+
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
+	public String getInsuranceStatus() {
+		return insuranceStatus;
+	}
+
+	public void setInsuranceStatus(String insuranceStatus) {
+		this.insuranceStatus = insuranceStatus;
+	}
+
+	public LocalDate getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(LocalDate paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+	
+	public MotorInsurance getMotorInsurance() {
+		return motorInsurance;
+	}
+
+	public void setMotorInsurance(MotorInsurance motorInsurance) {
+		this.motorInsurance = motorInsurance;
+	}
 }
