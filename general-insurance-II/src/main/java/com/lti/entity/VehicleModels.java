358be11a5fb064b9e4760fa1932e668b6a2e69c2
RajPriyaB <b.priyarenu@gmail.com>
2021-01-08 23:33:42 +0530

    added code in VehicleModels.java

--- general-insurance-II/src/main/java/com/lti/entity/VehicleModels.java
+++ general-insurance-II/src/main/java/com/lti/entity/VehicleModels.java
@@ -1,5 +1,42 @@
 package com.lti.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vehicle_model")
 public class VehicleModels {
+	
+	@Id
+	private int id;
+	private String manufacturer;
+	private String model;
+	private double price;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 }
