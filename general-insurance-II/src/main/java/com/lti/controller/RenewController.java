a17c75b758087c4b4265fba25fa4d42010d8f9b1
Rutvik <kulkarnirutvikvishwas@gmail.com>
2021-01-13 17:07:10 +0530

    Some changes

--- general-insurance-II/src/main/java/com/lti/controller/RenewController.java
+++ general-insurance-II/src/main/java/com/lti/controller/RenewController.java
@@ -20,7 +20,7 @@ import com.lti.service.UserService;
 @RestController
 @CrossOrigin
 public class RenewController {
-	
+
 	@Autowired
 	private UserService userService;
 
@@ -28,28 +28,26 @@ public class RenewController {
 	public @ResponseBody RenewStatus renew(@RequestBody RenewDetails renewDetails) {
 		try {
 			MotorInsurance motorInsurance = userService.getDetails(renewDetails);
-			if(motorInsurance.getPlanExpiryDate().compareTo(LocalDate.now()) <= 0) {
-			RenewStatus status = new RenewStatus();
-			status.setMotorInsurance(motorInsurance);
-			status.setStatus(StatusType.SUCCESS);
-			status.setMessage("User Found");
-			 
-			return status;
-			}	
-			else
-			{
+			if (motorInsurance.getPlanExpiryDate().compareTo(LocalDate.now()) <= 0) {
+				RenewStatus status = new RenewStatus();
+				status.setMotorInsurance(motorInsurance);
+				status.setStatus(StatusType.SUCCESS);
+				status.setMessage("User Found");
+
+				return status;
+			} else {
 				RenewStatus status = new RenewStatus();
 				status.setStatus(StatusType.FAILED);
-				status .setMessage("Insurance is not expired");
+				status.setMessage("Insurance is not expired");
 				status.setMotorInsurance(motorInsurance);
-				
+
 				return status;
-			}	
-		} catch(UserServiceException e) {
+			}
+		} catch (UserServiceException e) {
 			RenewStatus status = new RenewStatus();
 			status.setStatus(StatusType.FAILED);
 			status.setMessage(e.getMessage());
-			
+
 			return status;
 		}
 	}
--- general-insurance-II/src/main/java/com/lti/service/UserServiceImpl.java
+++ general-insurance-II/src/main/java/com/lti/service/UserServiceImpl.java
@@ -47,55 +47,54 @@ public class UserServiceImpl implements UserService {
 		} catch (NoResultException e) {
 			throw new UserServiceException("incorrect password");
 		}
-	}	
+	}
+
 	@Override
 	@Transactional
-	public InsuranceClaim claim(int policyNumber, String email, String password, String claimReason, double claimAmount) {
+	public InsuranceClaim claim(int policyNumber, String email, String password, String claimReason,
+			double claimAmount) {
 		try {
 			if (!userDao.isUserPresent(email))
 				throw new UserServiceException("User not registered");
 			else {
-					System.out.println("Email found");
-					if (userDao.findByEmailAndPassword(email, password)>0) {
-						System.out.println("Password found");
-						int userId = userDao.findByEmailAndPassword(email, password);
-						if (!userDao.isPolicyPresent(policyNumber, userId))
-							throw new UserServiceException("Incorrect policy number");
-						else {
-							System.out.println("Policy found");
-							if (userDao.findBalanceClaimAmount(policyNumber)>claimAmount) {
-								
-								System.out.println(userDao.findBalanceClaimAmount(policyNumber));
+				System.out.println("Email found");
+				if (userDao.findByEmailAndPassword(email, password) > 0) {
+					System.out.println("Password found");
+					int userId = userDao.findByEmailAndPassword(email, password);
+					if (!userDao.isPolicyPresent(policyNumber, userId))
+						throw new UserServiceException("Incorrect policy number");
+					else {
+						System.out.println("Policy found");
+						if (userDao.findBalanceClaimAmount(policyNumber) > claimAmount) {
 
-								InsuranceClaim insuranceClaim = new InsuranceClaim();
-								insuranceClaim.setClaimReason(claimReason);
-								insuranceClaim.setClaimDate(LocalDate.now());
-								insuranceClaim.setClaimStatus("APPROVED");
-								insuranceClaim.setClaimAmount(claimAmount);
-								
-								MotorInsurance motorInsurance = userDao.Fetch(MotorInsurance.class, policyNumber);
-								motorInsurance.setTotalClaimAmount(motorInsurance.getTotalClaimAmount() + claimAmount);
-								motorInsurance.setBalanceClaimAmount(motorInsurance.getBalanceClaimAmount() - claimAmount);
-								
-								insuranceClaim.setMotorInsurance(motorInsurance);
-								
-								MotorInsurance updatedmotorInsurance = (MotorInsurance) userDao.store(motorInsurance);
-								InsuranceClaim updatedInsuranceClaim = (InsuranceClaim) userDao.store(insuranceClaim);
-								return updatedInsuranceClaim;
-							}
-							else
-								throw new UserServiceException("Enough money not available for claim");
-						}
+							System.out.println(userDao.findBalanceClaimAmount(policyNumber));
+
+							InsuranceClaim insuranceClaim = new InsuranceClaim();
+							insuranceClaim.setClaimReason(claimReason);
+							insuranceClaim.setClaimDate(LocalDate.now());
+							insuranceClaim.setClaimStatus("APPROVED");
+							insuranceClaim.setClaimAmount(claimAmount);
+
+							MotorInsurance motorInsurance = userDao.Fetch(MotorInsurance.class, policyNumber);
+							motorInsurance.setTotalClaimAmount(motorInsurance.getTotalClaimAmount() + claimAmount);
+							motorInsurance.setBalanceClaimAmount(motorInsurance.getBalanceClaimAmount() - claimAmount);
+
+							insuranceClaim.setMotorInsurance(motorInsurance);
+
+							MotorInsurance updatedmotorInsurance = (MotorInsurance) userDao.store(motorInsurance);
+							InsuranceClaim updatedInsuranceClaim = (InsuranceClaim) userDao.store(insuranceClaim);
+							return updatedInsuranceClaim;
+						} else
+							throw new UserServiceException("Enough money not available for claim");
 					}
-					else
-						throw new UserServiceException("Incorrect password");
+				} else
+					throw new UserServiceException("Incorrect password");
 			}
-		}
-		catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new UserServiceException("Incorrect password");
 		}
 	}
-	
+
 	@Override
 	public List<VehicleModels> fetchVehicles() {
 		List<VehicleModels> models = userDao.fetchAll(VehicleModels.class);
@@ -105,11 +104,11 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public MotorInsurance getDetails(RenewDetails renewDetails) {
 		try {
-			if(!userDao.isUserPresent(renewDetails.getEmail()))
+			if (!userDao.isUserPresent(renewDetails.getEmail()))
 				throw new UserServiceException("User not registered");
-		    MotorInsurance motorInsurance = userDao.getInsuranceDetails(renewDetails);
-		    return motorInsurance;
-		} catch(NoResultException e) {
+			MotorInsurance motorInsurance = userDao.getInsuranceDetails(renewDetails);
+			return motorInsurance;
+		} catch (NoResultException e) {
 			throw new UserServiceException("incorrect mail id or policy number or phone number");
 		}
 	}
