93bba889b279784f8ed19de83eb44dab59ae671a
VasaviReddy <vasavireddyp1998@gmail.com>
2021-01-13 13:15:21 +0530

    added Renew part

--- general-insurance-II/src/main/java/com/lti/controller/RenewController.java
+++ general-insurance-II/src/main/java/com/lti/controller/RenewController.java
@@ -1,5 +1,7 @@
 package com.lti.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lti.dto.RenewDetails;
 import com.lti.dto.RenewStatus;
+import com.lti.dto.Status;
 import com.lti.dto.Status.StatusType;
 import com.lti.entity.MotorInsurance;
 import com.lti.exception.UserServiceException;
@@ -25,15 +28,29 @@ public class RenewController {
 	public @ResponseBody RenewStatus renew(@RequestBody RenewDetails renewDetails) {
 		try {
 			MotorInsurance motorInsurance = userService.getDetails(renewDetails);
-			
+			if(motorInsurance.getPlanExpiryDate().compareTo(LocalDate.now()) <= 0) {
 			RenewStatus status = new RenewStatus();
 			status.setMotorInsurance(motorInsurance);
 			status.setStatus(StatusType.SUCCESS);
 			status.setMessage("User Found");
 			 
 			return status;
+			}	
+			else
+			{
+				RenewStatus status = new RenewStatus();
+				status.setStatus(StatusType.FAILED);
+				status .setMessage("Insurance is not expired");
+				status.setMotorInsurance(motorInsurance);
+				
+				return status;
+			}	
 		} catch(UserServiceException e) {
-			return null;
+			RenewStatus status = new RenewStatus();
+			status.setStatus(StatusType.FAILED);
+			status.setMessage(e.getMessage());
+			
+			return status;
 		}
 	}
 }
