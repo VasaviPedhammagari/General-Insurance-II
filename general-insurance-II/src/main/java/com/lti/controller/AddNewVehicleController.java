2bbf015501e1305cadc320eec17dd7421342c3bd
raj3priya <b.priyarenu@gmail.com>
2021-01-15 13:19:22 +0530

    Updated add vehicleModel

--- general-insurance-II/src/main/java/com/lti/controller/AddNewVehicleController.java
+++ general-insurance-II/src/main/java/com/lti/controller/AddNewVehicleController.java
@@ -1,7 +1,10 @@
 package com.lti.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,6 +41,26 @@ public class AddNewVehicleController {
 			status.setMessage("Adding new model failed!");
 			return status;
 			
+		}
+	}
+	
+	@GetMapping("/listvehiclemodel")
+	public @ResponseBody VehicleModelStatus list() {
+		List<VehicleModels> vehicleList;
+		try {
+			vehicleList = vehicleService.listAvailableVehicle();
+			VehicleModelStatus status = new VehicleModelStatus();
+			status.setStatus(StatusType.SUCCESS);
+			status.setMessage("Fetched Successfully!");
+			status.setVehicleModels(vehicleList);
+			return status;
+		}
+		catch(VehicleModelServiceException e) {
+			e.printStackTrace();
+			VehicleModelStatus status = new VehicleModelStatus();
+			status.setStatus(StatusType.FAILED);
+			status.setMessage("Fetch Failed!");
+			return status;
 		}
 	}
 
--- general-insurance-II/src/main/java/com/lti/dto/VehicleModelStatus.java
+++ general-insurance-II/src/main/java/com/lti/dto/VehicleModelStatus.java
@@ -1,10 +1,13 @@
 package com.lti.dto;
 
+import java.util.List;
+
 import com.lti.entity.VehicleModels;
 
 public class VehicleModelStatus extends Status{
 	
 	private VehicleModels vehicleModel;
+	private List<VehicleModels> vehicleModels;
 
 	public VehicleModels getVehicleModel() {
 		return vehicleModel;
@@ -12,6 +15,14 @@ public class VehicleModelStatus extends Status{
 
 	public void setVehicleModel(VehicleModels vehicleModel) {
 		this.vehicleModel = vehicleModel;
+	}
+
+	public List<VehicleModels> getVehicleModels() {
+		return vehicleModels;
+	}
+
+	public void setVehicleModels(List<VehicleModels> vehicleModels) {
+		this.vehicleModels = vehicleModels;
 	} 
 	
 	
