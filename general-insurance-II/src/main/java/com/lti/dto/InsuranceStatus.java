5b84c682415c36ac8fdc6c509d186cb5bbc92473
VasaviReddy <vasavireddyp1998@gmail.com>
2021-01-18 17:16:52 +0530

    solved error in renewController

--- general-insurance-II/src/main/java/com/lti/dto/InsuranceStatus.java
+++ general-insurance-II/src/main/java/com/lti/dto/InsuranceStatus.java
@@ -1,10 +1,12 @@
 
 package com.lti.dto;
 
+import com.lti.entity.MotorInsurance;
 import com.lti.entity.Payment;
 
 public class InsuranceStatus extends Status{
 
+	private MotorInsurance motorInsurance;
 	private Payment payment;
 
 	public Payment getPayment() {
@@ -14,5 +16,13 @@ public class InsuranceStatus extends Status{
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
-	
+
+	public MotorInsurance getMotorInsurance() {
+		return motorInsurance;
+	}
+
+	public void setMotorInsurance(MotorInsurance motorInsurance) {
+		this.motorInsurance = motorInsurance;
+	}
+
 }
